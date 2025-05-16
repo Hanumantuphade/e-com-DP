7e4bba77315f25121a674aa7081449088e942cf2
Kvothe045 <aryansrivastav106@example.com>
2025-05-16 18:16:46 +0530

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
       <ServiceFeatures />
       <CustomerTestimonials />
       <ContactReview />
-      {/* <Footer /> */}
+      <Footer />
     </main>
   );
 }
